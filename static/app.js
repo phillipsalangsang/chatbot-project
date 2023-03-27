425cef8e73a7827e487ad8501109e09af8fa5a2b
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-03-27 15:54:36 +0100

    Hard coded port

--- static/app.js
+++ static/app.js
@@ -48,7 +48,7 @@ class Chatbox {
         this.messages.push(msg1);
 
         
-        fetch($SCRIPT_ROOT + '/predict', {
+            fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
